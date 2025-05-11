0b00a4475d5a6c45d95d12c7f525c643f8f1b608
Ziad Tamer <139783717+ziadtamer27@users.noreply.github.com>
2025-05-11 12:58:15 +0300

    Create echo.cpp

---
+++ echo.cpp
@@ -0,0 +1,16 @@
+
+int echo_main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("\n");
+    } else {
+       
+        for (int i = 1; i < argc; i++) {
+            printf("%s", argv[i]);
+            if (i < argc - 1) {
+                printf(" "); 
+            }
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
